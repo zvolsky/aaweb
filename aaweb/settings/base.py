3893152e349fc55325a6476c1ccd7616ebdb8d58
Mirek Zvolský <zvolsky@seznam.cz>
2020-02-19 20:11:21 +0100

    pokus

--- aaweb/settings/base.py
+++ aaweb/settings/base.py
@@ -272,8 +272,8 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = [
 #    os.path.join(PROJECT_DIR, 'static'),
 #]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'  # troubleshouting Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_ROOT = os.path.join(DEV_TMP_DIR, 'static', 'root')
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
