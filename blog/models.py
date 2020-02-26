4234ec91ccac33819d0728e21f276d519e4c5294
Mirek Zvolský <zvolsky@seznam.cz>
2020-02-26 12:47:23 +0100

    urls with tag&category, chapter 4

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
+import datetime
+
 from django import forms
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.snippets.models import register_snippet
@@ -12,7 +15,7 @@ from modelcluster.tags import ClusterTaggableManager
 from taggit.models import TaggedItemBase, Tag as TaggitTag
 
 
-class BlogPage(Page):
+class BlogPage(RoutablePageMixin, Page):  # order is important
     description = models.CharField(max_length=255, blank=True,)
 
     content_panels = Page.content_panels + [
@@ -23,20 +26,64 @@ class BlogPage(Page):
         verbose_name = _("Blog page")
         verbose_name_plural = _("Blog pages")
 
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context['posts'] = self.posts
+        context['blog_page'] = self
+        return context
 
-class PostPage(Page):
+    def get_posts(self):
+        return PostPage.objects.descendant_of(self).live()
+
+    @route(r'^tag/(?P<tag>[-\w]+)/$')
+    def post_by_tag(self, request, tag, *args, **kwargs):
+        self.search_type = 'tag'
+        self.search_term = tag
+        self.posts = self.get_posts().filter(tags__slug=tag)
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^category/(?P<category>[-\w]+)/$')
+    def post_by_category(self, request, category, *args, **kwargs):
+        self.search_type = 'category'
+        self.search_term = category
+        self.posts = self.get_posts().filter(categories__slug=category)
+        return Page.serve(self, request, *args, **kwargs)
+
+    @route(r'^$')
+    def post_list(self, request, *args, **kwargs):
+        self.posts = self.get_posts()
+        return Page.serve(self, request, *args, **kwargs)
+
+
+class PostPage(RoutablePageMixin, Page):
     body = RichTextField(blank=True)
+    date = models.DateTimeField(verbose_name="Post date", default=datetime.datetime.today)
     categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
     tags = ClusterTaggableManager(through='blog.BlogPageTag', blank=True)
+
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
         FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
         FieldPanel('tags'),
     ]
 
+    settings_panels = Page.settings_panels + [
+        FieldPanel('date'),
+    ]
+
     class Meta:
         verbose_name = _("Blog post page")
         verbose_name_plural = _("Blog post pages")
+
+    @property
+    def blog_page(self):
+        return self.get_parent().specific
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context['blog_page'] = self.blog_page
+        context['post'] = self
+        return context
 
 # --------------------------------- snippets
 
