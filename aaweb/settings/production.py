2fb3124ac2920755e0be0178d8df70ed2689cf09
Mirek Zvolský <zvolsky@seznam.cz>
2020-04-02 17:16:10 +0200

    production settings podle opentrafficweb

--- aaweb/settings/production.py
+++ aaweb/settings/production.py
@@ -36,6 +36,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'zvolsky@seznam.cz'
 EMAIL_HOST_PASSWORD = config.get('sendinblue', 'SMTP-PWD')
 EMAIL_USE_TLS = True
+ADMINS = (('Mirek', 'zvolsky@seznam.cz'), ('admin', 'aaweb.eu@gmail.com'))
 
 X_FRAME_OPTIONS = 'DENY'
 SECURE_HSTS_SECONDS = 300
@@ -52,3 +53,94 @@ try:
     from .local import *
 except ImportError:
     pass
+
+# https://stackoverflow.com/questions/21943962/how-to-see-details-of-django-errors-with-gunicorn
+LOGGING = {
+'version': 1,
+'disable_existing_loggers': False,
+'filters': {
+    'require_debug_false': {
+        '()': 'django.utils.log.RequireDebugFalse',
+    },
+    'require_debug_true': {
+        '()': 'django.utils.log.RequireDebugTrue',
+    },
+},
+'formatters': {
+    'django.server': {
+        '()': 'django.utils.log.ServerFormatter',
+        'format': '[%(server_time)s] %(message)s',
+    }
+},
+'handlers': {
+    'console': {
+        'level': 'INFO',
+        'filters': ['require_debug_true'],
+        'class': 'logging.StreamHandler',
+    },
+    # Custom handler which we will use with logger 'django'.
+    # We want errors/warnings to be logged when DEBUG=False
+    'console_on_not_debug': {
+        'level': 'WARNING',
+        'filters': ['require_debug_false'],
+        'class': 'logging.StreamHandler',
+    },
+    'django.server': {
+        'level': 'INFO',
+        'class': 'logging.StreamHandler',
+        'formatter': 'django.server',
+    },
+    'mail_admins': {
+        'level': 'ERROR',
+        'filters': ['require_debug_false'],
+        'class': 'django.utils.log.AdminEmailHandler'
+    },
+    'gunicorn': {
+        'level': 'DEBUG',
+        'class': 'logging.handlers.RotatingFileHandler',
+        'formatter': 'django.server',
+        'filename': '/home/www-data/dj/aaweb/aaweb/log/gunicorn/error.log',
+        'maxBytes': 1024 * 1024 * 32,  # 32 mb
+    }
+},
+'loggers': {
+    'django': {
+        'handlers': ['console', 'mail_admins', 'console_on_not_debug', 'gunicorn'],  # , 'gunicorn' (fails)
+        'level': 'INFO',
+    },
+    'django.server': {
+        'handlers': ['django.server'],
+        'level': 'INFO',
+        'propagate': False,
+    },
+}
+}
+
+'''
+# https://stackoverflow.com/questions/21943962/how-to-see-details-of-django-errors-with-gunicorn - jinak?
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+        }
+    },
+    'handlers': {
+        'gunicorn': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': '/home/www-data/dj/aaweb/aaweb/log/gunicorn/error.log',
+            'maxBytes': 1024 * 1024 * 32,  # 32 mb
+        }
+    },
+    'loggers': {
+        'gunicorn.errors': {
+            'level': 'DEBUG',
+            'handlers': ['gunicorn'],
+            'propagate': True,
+        },
+    }
+}
+'''
--- schemas_customers/urls.py
+++ schemas_customers/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import path
 
 from schemas_customers import views
 
